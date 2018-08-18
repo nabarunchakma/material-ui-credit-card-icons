daf402ccb38bf3524de8b00a2e355b671f5c24e4
Nabarun Chakma <nabarun.chakma@gmail.com>
2018-08-18 14:34:26 +1000

    fix: upgrade material ui version

--- src/index.js
+++ src/index.js
@@ -10,31 +10,31 @@
 
  import React from 'react';
 
- export Icon_AmericanExpress   from './Icon_AmericanExpress';
- export Icon_CreditCardOutline from './Icon_CreditCardOutline';
- export Icon_DinersClub        from './Icon_DinersClub';
- export Icon_Discover          from './Icon_Discover';
- export Icon_JCB               from './Icon_JCB';
- export Icon_MasterCard        from './Icon_MasterCard';
- export Icon_Visa              from './Icon_Visa';
+ export IconAmericanExpress   from './Icon_AmericanExpress';
+ export IconCreditCardOutline from './Icon_CreditCardOutline';
+ export IconDinersClub        from './Icon_DinersClub';
+ export IconDiscover          from './Icon_Discover';
+ export IconJCB               from './Icon_JCB';
+ export IconMasterCard        from './Icon_MasterCard';
+ export IconVisa              from './Icon_Visa';
 
 
- import Icon_AmericanExpress   from './Icon_AmericanExpress';
- import Icon_CreditCardOutline from './Icon_CreditCardOutline';
- import Icon_DinersClub        from './Icon_DinersClub';
- import Icon_Discover          from './Icon_Discover';
- import Icon_JCB               from './Icon_JCB';
- import Icon_MasterCard        from './Icon_MasterCard';
- import Icon_Visa              from './Icon_Visa';
+ import IconAmericanExpress   from './Icon_AmericanExpress';
+ import IconCreditCardOutline from './Icon_CreditCardOutline';
+ import IconDinersClub        from './Icon_DinersClub';
+ import IconDiscover          from './Icon_Discover';
+ import IconJCB               from './Icon_JCB';
+ import IconMasterCard        from './Icon_MasterCard';
+ import IconVisa              from './Icon_Visa';
 
 
 export function getCreditCardIconByName( iconName )
 {
-  if( iconName == 'AmericanExpress' ) { return <Icon_AmericanExpress /> }
-  if( iconName == 'DinersClub' )      { return <Icon_DinersClub /> }
-  if( iconName == 'Discover' )        { return <Icon_Discover /> }
-  if( iconName == 'JCB' )             { return <Icon_JCB /> }
-  if( iconName == 'MasterCard' )      { return <Icon_MasterCard /> }
-  if( iconName == 'Visa' )            { return <Icon_Visa /> }
-                                        return <Icon_CreditCardOutline />;
+  if( iconName == 'AmericanExpress' ) { return <IconAmericanExpress /> }
+  if( iconName == 'DinersClub' )      { return <IconDinersClub /> }
+  if( iconName == 'Discover' )        { return <IconDiscover /> }
+  if( iconName == 'JCB' )             { return <IconJCB /> }
+  if( iconName == 'MasterCard' )      { return <IconMasterCard /> }
+  if( iconName == 'Visa' )            { return <IconVisa /> }
+                                        return <IconCreditCardOutline />;
 }
